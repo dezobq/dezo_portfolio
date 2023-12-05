3dc47a3bcfb8d12ad318efb0b0e2c3c5451cb65e
dezobq <84927483+dezobq@users.noreply.github.com>
2023-12-05 16:02:02 -0500

    testing cool stuff for my skill section

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -5,11 +5,71 @@ import Meter1 from '../assets/img/meter1.svg';
 import Meter2 from '../assets/img/meter2.svg';
 import Meter3 from '../assets/img/meter3.svg';
 import colorSharp from '../assets/img/color-sharp.png'
+import Marquee from "react-fast-marquee";
+import Techs from './Techs';
 
 const Skills = () => {
+
+
+  const techSkills = [
+    {
+      id: '1',
+      skill: 'MongoDB',
+      img: '../assets/img/skills/mongoDB.svg',
+    },
+    {
+      id: '2',
+      skill: 'NestJS',
+      img: '../assets/img/skills/nestJS.svg',
+    },
+    {
+      id: '3',
+      skill: 'React',
+      img: '../assets/img/skills/react.svg',
+    },
+    {
+      id: '4',
+      skill: 'NodeJS',
+      img: '../assets/img/skills/nodeJS.svg',
+    },
+    {
+      id: '5',
+      skill: 'Material UI',
+      img: '../assets/img/skills/materialUI.svg',
+    },
+    {
+      id: '6',
+      skill: 'javaScript',
+      img: '../assets/img/skills/js.svg',
+    },
+    {
+      id: '7',
+      skill: 'express',
+      img: '../assets/img/skills/expressJS.svg',
+    },
+    {
+      id: '8',
+      skill: 'HTML',
+      img: '../assets/img/skills/html.svg',
+    },
+    {
+      id: '9',
+      skill: 'Typescript',
+      img: '../assets/img/skills/ts.svg',
+    },
+    {
+      id: '10',
+      skill: 'CSS',
+      img: '../assets/img/skills/css.svg',
+    },
+    {
+      id: '11',
+      skill: 'Tailwind',
+      img: '../assets/img/skills/tailwind.svg',
+    },
+  ];
   const responsive = {
     superLargeDesktop: {
-      // the naming can be any, depends on you.
       breakpoint: { max: 4000, min: 3000 },
       items: 5
     },
@@ -33,7 +93,10 @@ const Skills = () => {
         <Row>
         <Col>
            <div className='skill-bx'>
+           
+
             <h2>My Skills</h2>
+            
             <p>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
               quod, voluptate, voluptatum, quibusdam, amet quas similique
@@ -41,24 +104,11 @@ const Skills = () => {
               voluptate, voluptatum, quibusdam, amet quas similique voluptas
               voluptatibus nesciunt magni voluptatem.
             </p>
-            <Carousel responsive={responsive} infinite={true} className='skill-slider'>
-              <div className='item'>
-                <img src={Meter1} alt="" />
-                <h5>Web Development</h5>
-              </div>
-              <div className='item'>
-                <img src={Meter2} alt="" />
-                <h5>Brand Identity</h5>
-              
-              </div>
-              <div className='item'>
-                <img src={Meter3} alt="" />
-                <h5>UI/UX Design</h5>
-              
-              </div>
-           
 
-            </Carousel>
+            {/*  {techSkills.map(({ id, skill, img }) => (
+                <Techs key={id} srcImg={img} skill={skill} />
+              ))} */}
+            
            </div>
         
         </Col>
