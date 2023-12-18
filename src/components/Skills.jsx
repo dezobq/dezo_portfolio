34edd122afee1e95bd5db7780cdafe1e52f3e3e9
dezobq <84927483+dezobq@users.noreply.github.com>
2023-12-18 13:02:46 -0500

    revamped skill component with techs carroussel

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,120 +1,121 @@
-import Carousel from 'react-multi-carousel';
-import 'react-multi-carousel/lib/styles.css';
-import { Container, Row, Col } from 'react-bootstrap';
-import Meter1 from '../assets/img/meter1.svg';
-import Meter2 from '../assets/img/meter2.svg';
-import Meter3 from '../assets/img/meter3.svg';
-import colorSharp from '../assets/img/color-sharp.png'
-import Marquee from "react-fast-marquee";
-import Techs from './Techs';
+import React from 'react'
+import Carousel from 'react-multi-carousel'
+import 'react-multi-carousel/lib/styles.css'
+import Techs from './Techs'
+
+const skillTechs = [
+  {
+    id: '1',
+    skill: 'React',
+    img: '/skills/react.svg'
+  },
+  {
+    id: '2',
+    skill: 'NestJS',
+    img: '/skills/nestJS.svg'
+  },
+  {
+    id: '3',
+    skill: 'React',
+    img: '/skills/react.svg'
+  },
+  {
+    id: '4',
+    skill: 'NodeJS',
+    img: '/skills/nodeJS.svg'
+  },
+  {
+    id: '5',
+    skill: 'Material UI',
+    img: '/skills/materialUI.svg'
+  },
+  {
+    id: '6',
+    skill: 'javaScript',
+    img: '/skills/js.svg'
+  },
+  {
+    id: '7',
+    skill: 'express',
+    img: '/skills/expressJS.svg'
+  },
+  {
+    id: '8',
+    skill: 'HTML',
+    img: '/skills/html.svg'
+  },
+  {
+    id: '9',
+    skill: 'Typescript',
+    img: '/skills/ts.svg'
+  },
+  {
+    id: '10',
+    skill: 'CSS',
+    img: '/skills/css.svg'
+  },
+  {
+    id: '11',
+    skill: 'Tailwind',
+    img: '/skills/tailwind.svg'
+  },
+]
+
+const responsive = {
+  superLargeDesktop: {
+    // the naming can be any, depends on you.
+    breakpoint: { max: 4000, min: 3000 },
+    items: 5
+  },
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 3
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 2
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1
+  }
+}
 
 const Skills = () => {
-
-
-  const techSkills = [
-    {
-      id: '1',
-      skill: 'MongoDB',
-      img: '../assets/img/skills/mongoDB.svg',
-    },
-    {
-      id: '2',
-      skill: 'NestJS',
-      img: '../assets/img/skills/nestJS.svg',
-    },
-    {
-      id: '3',
-      skill: 'React',
-      img: '../assets/img/skills/react.svg',
-    },
-    {
-      id: '4',
-      skill: 'NodeJS',
-      img: '../assets/img/skills/nodeJS.svg',
-    },
-    {
-      id: '5',
-      skill: 'Material UI',
-      img: '../assets/img/skills/materialUI.svg',
-    },
-    {
-      id: '6',
-      skill: 'javaScript',
-      img: '../assets/img/skills/js.svg',
-    },
-    {
-      id: '7',
-      skill: 'express',
-      img: '../assets/img/skills/expressJS.svg',
-    },
-    {
-      id: '8',
-      skill: 'HTML',
-      img: '../assets/img/skills/html.svg',
-    },
-    {
-      id: '9',
-      skill: 'Typescript',
-      img: '../assets/img/skills/ts.svg',
-    },
-    {
-      id: '10',
-      skill: 'CSS',
-      img: '../assets/img/skills/css.svg',
-    },
-    {
-      id: '11',
-      skill: 'Tailwind',
-      img: '../assets/img/skills/tailwind.svg',
-    },
-  ];
-  const responsive = {
-    superLargeDesktop: {
-      breakpoint: { max: 4000, min: 3000 },
-      items: 5
-    },
-    desktop: {
-      breakpoint: { max: 3000, min: 1024 },
-      items: 3
-    },
-    tablet: {
-      breakpoint: { max: 1024, min: 464 },
-      items: 2
-    },
-    mobile: {
-      breakpoint: { max: 464, min: 0 },
-      items: 1
-    }
-  };
   return (
-   
-    <section className='skill' id='skills'>
-      <Container>
-        <Row>
-        <Col>
-           <div className='skill-bx'>
-           
-
-            <h2>My Skills</h2>
-            
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
-              quod, voluptate, voluptatum, quibusdam, amet quas similique
-             
-            </p>
-
-            {/*  {techSkills.map(({ id, skill, img }) => (
-                <Techs key={id} srcImg={img} skill={skill} />
-              ))} */}
-            
-           </div>
-        
-        </Col>
-        </Row>
-        
-      </Container>
-      <img className='background-image-left' src={colorSharp} alt="" />
-
+    <section className="skill" id="skills">
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <div className="skill-bx wow zoomIn">
+              <h2>Skills</h2>
+              <h3>
+                A full-stack web developer with passion for the newest techs
+              </h3>
+              <p>
+                I have maintained, developed and launched multiple projects from
+                scratch, handling the development of their back-end and
+                front-end codebases.
+                <br />
+                Lately, I find myself increasingly reliant on low-code/no-code
+                tools like Plasmic and Xano. They facilitate high-quality
+                project delivery with immense flexibility, all at a
+                significantly reduced cost.
+              </p>
+              <Carousel
+                responsive={responsive}
+                infinite={true}
+                className="owl-carousel owl-theme skill-slider"
+                autoPlay
+              >
+                {skillTechs.map(({ id, skill, img }) => (
+                  <Techs key={id} skill={skill} srcImg={img} />
+                ))}
+              </Carousel>
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
   )
 }
--- src/components/Techs.jsx
+++ src/components/Techs.jsx
@@ -1,18 +1,14 @@
 import React from 'react'
 
-const Techs = (skill, srcImg) => {
+const Techs = ({ skill, srcImg }) => {
   return (
-    <div className=" min-w-[100px]  my-2 md:mr-8 flex md:flex-col justify-center items-center">
-    <img
-      className="block w-[30px] h-[30px] lg:w-[40px] lg:h-[40px] object-contain hover:scale-125 transition-all duration-200"
-      src={srcImg}
-    />
-    <p className=" tracking-widest font-semibold text-[.625rem] md:text-xs">
-      {skill}
-    </p>
-  </div>
+    <div className="item">
+      <img src={srcImg} />
+      <p className=" tracking-widest font-semibold text-[.625rem] md:text-xs">
+        {skill}
+      </p>
+    </div>
   )
-  
 }
 
 export default Techs
